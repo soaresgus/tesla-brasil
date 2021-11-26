a19db6371eee73aa6bf3c202fd919901b525d981
Gustavo <gustavosoaresbt@hotmail.com>
2021-11-26 11:33:31 -0300

    Versão final

--- js/produtos.js
+++ js/produtos.js
@@ -1,4 +1,12 @@
+let primeiro = 1;
+
 function imagem(elemento) {
+    if(document.querySelector('.primeiro').src == elemento.src) {
+        primeiro=1;
+    }else {
+        primeiro=0;
+    }
+
     if (document.querySelector('#geral').src !== elemento.src) {
         document.querySelector('#geral').style.opacity = '0';
 
@@ -10,30 +18,38 @@ function imagem(elemento) {
 
         const imgs = document.querySelectorAll('.img');
 
-        for(var i = 0; i < document.querySelectorAll('.img').length; i++) {
+        for (var i = 0; i < document.querySelectorAll('.img').length; i++) {
             const imgs = document.querySelectorAll('.img')[i];
             imgs.style.borderColor = '#f5f5f5';
         }
         elemento.style.borderColor = '#393C41';
     }
-    
+
 }
 
-function moeda(moeda) {
-    
-    if(moeda == "usd") {
-        document.querySelector('#valor').innerHTML = '53.900'
+function moeda(moeda, valor) {
+
+    if (moeda == "usd") {
+        document.querySelector('#valor').innerHTML = valor
         document.querySelector('#moeda').innerHTML = ' USD'
     }
 
-    if(moeda == "eur") {
-        document.querySelector('#valor').innerHTML = '47.700'
+    if (moeda == "eur") {
+        document.querySelector('#valor').innerHTML = valor
         document.querySelector('#moeda').innerHTML = ' EUR'
     }
 
-    if(moeda == "brl") {
-        document.querySelector('#valor').innerHTML = '302.500'
+    if (moeda == "brl") {
+        document.querySelector('#valor').innerHTML = valor
         document.querySelector('#moeda').innerHTML = ' BRL'
+    }
+
+}
+
+function cor(cor, carro, extensao) {
+    document.querySelector('.primeiro').src = 'img/' + carro + '/cores/' + cor + '.' + extensao;
+    if (primeiro == 1) {
+        document.querySelector('#geral').src = 'img/' + carro + '/cores/' + cor + '.' + extensao;
     }
 
 }
